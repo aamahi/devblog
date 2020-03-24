ce83430db9435277f39bdb40fcf5112b58364415
mahi <amahi.wp@gmail.com>
2020-03-25 04:51:08 +0600

    Modify some frontend

---
+++ resources/views/frontend/footer.blade.php
@@ -0,0 +1,11 @@
+<script src="{{asset('frontend/assets/plugins/jquery-3.3.1.min.js')}}"></script>
+<script src="{{asset('frontend/assets/plugins/popper.min.js')}}"></script>
+<script src="{{asset('frontend/assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+
+<!-- Style Switcher (REMOVE ON YOUR PRODUCTION SITE) -->
+<script src="{{asset('frontend/assets/js/demo/style-switcher.js')}}"></script>
+
+
+</body>
+</html>
+
