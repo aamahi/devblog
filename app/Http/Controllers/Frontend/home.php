7fbd930875df4aa23acc3930e0e819ee37f026a5
mahi <amahi.wp@gmail.com>
2020-03-31 05:02:12 +0600

    Cache Problem solved

--- app/Http/Controllers/Frontend/home.php
+++ app/Http/Controllers/Frontend/home.php
@@ -9,9 +9,15 @@ use Illuminate\Http\Request;
 class home extends Controller
 {
     public function index(){
+//        $all_post = cache('all_post',function (){
+//                return Post::with('category')->select('id','category_id','title','post_details','image','status','author','created_at')->ORDERBY('id','DESC')->take(5)->get();
+//        });
         $all_post = Post::with('category')->select('id','category_id','title','post_details','image','status','author','created_at')->ORDERBY('id','DESC')->paginate(5);
         return view('frontend.index',compact('all_post'));
     }
+
+
+
     public function frontend_post_details($id){
         $single_post = Post::find($id);
         return view('frontend.frontend_post_details',compact('single_post'));
